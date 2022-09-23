81769e2aeab1f500a6f68685ad04de3d3e828846
davi <davijluna@gmail.com>
2022-09-23 17:47:43 -0300

    refactor: retirando archetype da exportacao

--- src/Races/index.ts
+++ src/Races/index.ts
@@ -3,9 +3,8 @@ import Orc from './Orc';
 import Elf from './Elf';
 import Halfling from './Halfling';
 import Dwarf from './Dwarf';
-import Archetype from '../ Archetypes/Archetype';
 
 export default Race;
 // export default Archetype;
 
-export { Orc, Dwarf, Elf, Halfling, Archetype };
+export { Orc, Dwarf, Elf, Halfling };
