d37e1d9000420e0ffb93a731e59022bc328efd32
davi <davijluna@gmail.com>
2022-09-23 17:39:30 -0300

    refactor: comentando linha 9 para tirar erro de arquivo index

--- src/Races/index.ts
+++ src/Races/index.ts
@@ -3,6 +3,9 @@ import Orc from './Orc';
 import Elf from './Elf';
 import Halfling from './Halfling';
 import Dwarf from './Dwarf';
+import Archetype from '../ Archetypes/Archetype';
 
 export default Race;
-export { Orc, Dwarf, Elf, Halfling };
+// export default Archetype;
+
+export { Orc, Dwarf, Elf, Halfling, Archetype };
