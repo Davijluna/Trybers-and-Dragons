88aa57e75ffa2fa468262b2e6f72f300d8a474ac
davi <davijluna@gmail.com>
2022-09-22 23:12:29 -0300

    refactor: dicionando import das class orc halfling elf dwarf

--- src/Races/index.ts
+++ src/Races/index.ts
@@ -1,3 +1,8 @@
 import Race from './Race';
+import Orc from './Orc';
+import Elf from './Elf';
+import Halflind from './Halfling';
+import Dwarf from './Dwarf';
 
 export default Race;
+export { Orc, Elf, Halflind, Dwarf };
