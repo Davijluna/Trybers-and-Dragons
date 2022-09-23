9dcb4317ba79f2213b5226b747e1fddbf814d5bc
davi <davijluna@gmail.com>
2022-09-23 01:50:12 -0300

    refactor: adicionando os import de maneira correta no arquivo index

--- src/Races/index.ts
+++ src/Races/index.ts
@@ -1,8 +1,8 @@
 import Race from './Race';
 import Orc from './Orc';
 import Elf from './Elf';
-import Halflind from './Halfling';
+import Halfling from './Halfling';
 import Dwarf from './Dwarf';
 
 export default Race;
-export { Orc, Elf, Halflind, Dwarf };
+export { Orc, Dwarf, Elf, Halfling };
